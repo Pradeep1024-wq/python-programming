be6bb92c84e5c7375327a1e140e11f721a3291ad
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-11-27 22:33:56 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,18 @@
+p= input('Enter first number: ')  
+n = input('Enter second number: ')  
+
+sum = float(p) + float(n)  
+  
+min = float(p) - float(n)  
+ 
+mul = float(p) * float(n)  
+
+div = float(p) / float(n)  
+ 
+print('The sum of {0} and {1} is {2}'.format(p, n, sum))  
+  
+print('The subtraction of {0} and {1} is {2}'.format(p, n, min))  
+
+print('The multiplication of {0} and {1} is {2}'.format(p, n, mul))  
+
+print('The division of {0} and {1} is {2}'.format(p, n, div))  
